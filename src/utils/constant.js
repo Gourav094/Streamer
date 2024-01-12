72361484b26d403eed9287a6e1048a7f6104e128
gourav garg <garggourav012@gmail.com>
2024-01-12 22:33:01 +0530

    fix: issue

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -11,7 +11,7 @@ export const VIDEO_DATA = (videoId) => {
     )
 }
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://corsproxy.org/?http%3A%2F%2Fsuggestqueries.google.com%2Fcomplete%2Fsearch%3Fclient%3Dfirefox%26ds%3Dyt%26q=";
 
 export const SEARCH_VIDEOS = (Query) => {
     return `https://youtube.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=25&q=${Query}&key=${API_KEY}`
